6c85a18232a48bd01b7a54ce086b8a9b1db31cb3
mhoopmann <michael.hoopmann@systemsbiology.org>
2015-08-14 13:54:50 -0700

    Updating version to 1.4.0, and release date, for official release

--- Kojak.cpp
+++ Kojak.cpp
@@ -24,7 +24,7 @@ bool getBaseFileName(string& base, char* fName);
 
 int main(int argc, char* argv[]){
 
-  cout << "Kojak version 1.3.7-dev, July 28 2015" << endl;
+  cout << "Kojak version 1.4.0, August 14 2015" << endl;
   cout << "Copyright Michael Hoopmann, Institute for Systems Biology" << endl;
   if(argc<2){
     cout << "Usage: Kojak <Config File> [<Data File>...]" << endl;
@@ -77,7 +77,7 @@ int main(int argc, char* argv[]){
   
   //Step #3: Read in spectra and map precursors
   KData spec(&params);
-  spec.setVersion("1.3.7-dev");
+  spec.setVersion("1.4.0");
   for(i=0;i<params.xLink->size();i++) spec.setLinker(params.xLink->at(i));
 
   //Iterate over all input files
